e0208e304b07752e252d883951d5cbc178a5cf6b
Martin Chabot <chabotm@gmail.com>
2012-08-28 17:24:36 -0400

    Improving minitest unit tests

--- test/gemgen_test.rb
+++ test/gemgen_test.rb
@@ -3,16 +3,21 @@ require 'gemgen/generator'
 
 module Gemgen
   class GemgenTest < MiniTest::Unit::TestCase
-    def test_minitest_unit
+    def setup
       if File.exist? tmp_dir
         rm_rf tmp_dir
       end
       mkdir tmp_dir
+    end
+
+    def test_that_minitest_unit_tests_runs
+      gem_name = 'minitest_unit'
       Dir.chdir(tmp_dir) do |dir|
-        @cli = Gemgen::Generator.new(['tata'])
+        @cli = Gemgen::Generator.new([gem_name])
         @cli.invoke_all
-        Dir.chdir('tata') do
-          raise "rake test execution failed" unless system("bundle exec rake") 
+        Dir.chdir(gem_name) do
+          output = `bundle exec rake`
+          assert_match /1 tests, 1 assertions, 0 failures, 0 errors, 0 skips/, output
         end
       end
     end
